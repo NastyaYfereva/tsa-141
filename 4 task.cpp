7203abc94bbcd7caef4e10692a83361508d8306a
Настя Юферева ТСА-141 <Nastyayfereva@gmail.com>
2025-05-16 17:35:46 +0300

    Add files via upload

---
+++ 4 task.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <cmath>
+#include <limits>
+using namespace std;
+
+/**
+ * @brief Считывает значения с клавиатуры с проверкой ввода
+ * @return возвращает значение, если оно правильное, иначе завершает программу
+ */
+double getValue();
+
+/**
+ * @brief Функция для вычисления значения функции y = ln(1/(2x + x²))
+ * @param x - аргумент функции
+ * @return значение функции
+ * @throws runtime_error если вычисление невозможно
+ */
+double calculateFunction(const double x);
+
+/**
+ * @brief Функция для проверки, что шаг положительный
+ * @return шаг
+ */
+double getPositiveStep();
+
+/**
+ * @brief Точка входа в программу
+ * @return 0 в случае успешного выполнения, 1 при ошибке
+ */
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    cout << "Табулирование функции y = ln(1/(2x + x²))" << endl;
+    cout << "Интервал x ∈ [-2, 0], шаг 0.2 (по условию задания 16)" << endl;
+
+    // Фиксированные параметры из задания
+    double startX = -2.0;
+    double endX = 0.0;
+    double step = 0.2;
+
+    cout << "\nРезультаты табулирования:" << endl;
+    cout << "x\t|\ty" << endl;
+    cout << "-----------------------" << endl;
+
+    for (double x = startX; x <= endX + 1e-9; x += step)
+    {
+        try {
+            double y = calculateFunction(x);
+            cout << x << "\t|\t" << y << endl;
+        } 
+        catch (const runtime_error& e) {
+            cout << x << "\t|\t" << "Ошибка: " << e.what() << endl;
+        }
+    }
+
+    return 0;
+}
+
+double getValue()
+{
+    double value;
+    cin >> value;
+    if (cin.fail())
+    {
+        cout << "Некорректное значение" << endl;
+        exit(1);
+    }
+    return value;
+}
+
+double calculateFunction(const double x)
+{
+    double denominator = 2 * x + pow(x, 2);
+    if (denominator >= 0) {
+        throw runtime_error("аргумент логарифма должен быть отрицательным (2x + x² < 0)");
+    }
+    return log(1 / denominator);
+}
+
+double getPositiveStep()
+{
+    double step;
+    cout << "Введите шаг: ";
+    step = getValue();
+    if (step <= 0)
+    {
+        cout << "Ошибка. Шаг должен быть положительным." << endl;
+        exit(1);
+    }
+    return step;
+}
