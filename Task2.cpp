0125a803bc2b59732bf6fecc4bad0184c3ac7b94
Настя Юферева ТСА-141 <Nastyayfereva@gmail.com>
2025-05-31 09:36:01 +0300

    Update Task2.cpp

--- Task2.cpp
+++ Task2.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
 
+/**
+ * @brief Пространство имен стандартной библиотеки C++
+ */
 using namespace std;
 
-double convertToInches(double cm);
+/**
+ * @brief Конвертирует сантиметры в дюймы
+ * @param cm Длина в сантиметрах
+ * @return Длина в дюймах
+ */
+double convertToInches(const double cm);
 
 /**
  * @brief Точка входа в программу
- * @return 0 в случае успешного выполнения
+ * @return Код возврата: 0 - успешное выполнение, 1 - ошибка ввода
+ * @details Программа запрашивает у пользователя длину в сантиметрах
+ * и выводит эквивалентное значение в дюймах (1 дюйм = 2.5 см)
  */
 int main() 
 {
@@ -31,10 +41,11 @@ int main()
 
 /**
  * @brief Конвертирует сантиметры в дюймы
- * @param cm Длина в сантиметрах
+ * @param cm Длина в сантиметрах (константный параметр)
  * @return Длина в дюймах
+ * @details Используется коэффициент перевода: 1 дюйм = 2.5 см
  */
-double convertToInches(double cm) 
+double convertToInches(const double cm) 
 {
-    return cm / 2.5; // 1 дюйм = 2.5 см
+    return cm / 2.5;
 }
